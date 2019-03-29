6d14088b4512f2f6e176a9ba8fed154a5cd4996c
Aiden Thinn (RIT Student) <mzt5441@ad.rit.edu>
2019-03-28 20:45:31 -0400

    finished domo maker B

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -11,26 +11,25 @@ const signupPage = (req, res) => {
 };
 
 const logout = (req, res) => {
+  req.session.destroy();
   res.redirect('/');
 };
 
 const login = (request, response) => {
   const req = request;
   const res = response;
-
-    // force cast to strings to cover some security flaws
+  // force cast to strings to cover some security flaws
   const username = `${req.body.username}`;
   const password = `${req.body.pass}`;
 
   if (!username || !password) {
-    return res.status(400).json({ error: 'RAWR! All fields are required' });
+    return res.status(400).json({ error: 'All fields are required.' });
   }
-
   return Account.AccountModel.authenticate(username, password, (err, account) => {
     if (err || !account) {
       return res.status(401).json({ error: 'Wrong username or password' });
     }
-
+    req.session.account = Account.AccountModel.toAPI(account);
     return res.json({ redirect: '/maker' });
   });
 };
@@ -39,17 +38,16 @@ const signup = (request, response) => {
   const req = request;
   const res = response;
 
-    // cast to strings to cover up some security flaws
   req.body.username = `${req.body.username}`;
   req.body.pass = `${req.body.pass}`;
   req.body.pass2 = `${req.body.pass2}`;
 
   if (!req.body.username || !req.body.pass || !req.body.pass2) {
-    return res.status(400).json({ error: 'RAWR! All fields are required' });
+    return res.status(400).json({ error: 'All fields are required' });
   }
 
   if (req.body.pass !== req.body.pass2) {
-    return res.status(400).json({ error: 'RAWR! Passwords do not match' });
+    return res.status(400).json({ error: 'Passwords do not match' });
   }
 
   return Account.AccountModel.generateHash(req.body.pass, (salt, hash) => {
@@ -58,21 +56,20 @@ const signup = (request, response) => {
       salt,
       password: hash,
     };
-
     const newAccount = new Account.AccountModel(accountData);
-
     const savePromise = newAccount.save();
 
-    savePromise.then(() => res.json({ redirect: '/maker' }));
+    savePromise.then(() => {
+      req.session.account = Account.AccountModel.toAPI(newAccount);
+      res.json({ redirect: '/maker' });
+    });
 
     savePromise.catch((err) => {
       console.log(err);
-
       if (err.code === 11000) {
-        return res.status(400).json({ error: 'Username already in use. ' });
+        return res.status(400).json({ error: 'Username already in use.' });
       }
-
-      return res.status(400).json({ error: 'An error occurred' });
+      return res.status(400).json({ error: 'An unexpected error occurred.' });
     });
   });
 };
@@ -82,4 +79,3 @@ module.exports.login = login;
 module.exports.logout = logout;
 module.exports.signupPage = signupPage;
 module.exports.signup = signup;
-
